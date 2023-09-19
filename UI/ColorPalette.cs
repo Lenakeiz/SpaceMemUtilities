916e7a99e9230673eae0e66e4ffdf08c30a6b6b4
Lenakeiz_UCL <andrea.castegnaro@outlook.com>
2023-09-19 18:48:46 +0100

    Added color palette

--- UI/ColorPalette.cs
+++ UI/ColorPalette.cs
@@ -11,7 +11,11 @@ namespace SpaceMem.UI
             new Color(173f / 255f, 216f / 255f, 230f / 255f, 1f), 
             new Color(152f / 255f, 251f / 255f, 152f / 255f, 1f),
             new Color(255f / 255f, 218f / 255f, 185f / 255f, 1f),
-            new Color(230f / 255f, 230f / 255f, 250f / 255f, 1f)
+            new Color(230f / 255f, 230f / 255f, 250f / 255f, 1f),
+            new Color(255f / 255f, 182f / 255f, 193f / 255f, 1f), // Pastel Pink
+            new Color(176f / 255f, 224f / 255f, 230f / 255f, 1f), // Powder Blue
+            new Color(255f / 255f, 255f / 255f, 224f / 255f, 1f), // Pale Yellow
+            new Color(216f / 255f, 191f / 255f, 216f / 255f, 1f)  // Thistle
         };
     }
 }
