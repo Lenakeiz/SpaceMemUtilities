df8dfbc55a1f410d8d8bd2ddb3a787134370ef62
Andrea <andrea.castegnaro@outlook.com>
2023-11-11 17:22:06 +0000

    Added structure for aes encryption class and added a static classs to generate compatible key and iv to be stored elsewhere

--- Encryption/Encryptor.cs
+++ Encryption/Encryptor.cs
@@ -1,18 +1,47 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
 
-public class Encr : MonoBehaviour
+namespace SpaceMem.Encryption
 {
-    // Start is called before the first frame update
-    void Start()
+    public class AesEncryptor : IEncryptor
     {
-        
-    }
+        private readonly byte[] _key;
+        private readonly byte[] _iv;
+        private readonly Aes _aes;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        // Constructor accepting external key and IV
+        public AesEncryptor(byte[] key, byte[] iv)
+        {
+            _key = key ?? throw new ArgumentNullException(nameof(key));
+            _iv = iv ?? throw new ArgumentNullException(nameof(iv));
+
+            if (_key.Length != 32) // 32 bytes for AES-256
+            {
+                throw new ArgumentException("Key must be 32 bytes for AES-256.", nameof(key));
+            }
+
+            if (_iv.Length != 16) // 16 bytes for IV
+            {
+                throw new ArgumentException("IV must be 16 bytes.", nameof(iv));
+            }
+
+            _aes = Aes.Create();
+            _aes.Key = _key;
+            _aes.IV = _iv;
+        }
+
+        public byte[] Encrypt(string data)
+        {
+            // ... Encryption logic remains the same
+            return null;
+        }
+
+        public string Decrypt(byte[] encryptedData)
+        {
+            // ... Decryption logic remains the same
+            return "";
+        }
     }
 }
