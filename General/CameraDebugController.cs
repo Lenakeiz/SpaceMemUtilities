c11177be89bbc9018bcdfa06446dd36d6cb15622
Andrea <andrea.castegnaro@outlook.com>
2023-12-21 00:25:46 +0000

    Updated Camera Controller

--- General/CameraDebugController.cs
+++ General/CameraDebugController.cs
@@ -10,7 +10,10 @@ namespace SpaceMem.General
         private float _pitch = 0.0f;  // Pitch of the camera (looking up and down)
         private float _yaw = 0.0f;  // Yaw of the camera (looking left and right)
 
-        private Vector3 moveDirection;
+        void Start()
+        {
+
+        }
 
         void Update()
         {
@@ -18,25 +21,15 @@ namespace SpaceMem.General
             float moveHorizontal = Input.GetAxisRaw("Horizontal");
             float moveVertical = Input.GetAxisRaw("Vertical");
 
-            // Calculate movement direction
-            moveDirection = (transform.right * moveHorizontal + transform.forward * moveVertical).normalized;
+            // Calculate movement direction based on parent's local axes
+            Vector3 moveDirection = (transform.right * moveHorizontal + transform.forward * moveVertical).normalized;
 
-            // Apply movement
-            transform.position += moveDirection * speed * Time.deltaTime;
+            // Apply movement in local space
+            transform.localPosition += moveDirection * speed * Time.deltaTime;
 
             // Get mouse input
             float mouseX = Input.GetAxis("Mouse X") * sensitivity;
             float mouseY = Input.GetAxis("Mouse Y") * sensitivity;
-
-            // Move up and down
-            if (Input.GetKey(KeyCode.Q))
-            {
-                transform.position += Vector3.up * speed * Time.deltaTime;
-            }
-            if (Input.GetKey(KeyCode.E))
-            {
-                transform.position -= Vector3.up * speed * Time.deltaTime;
-            }
 
             // Adjust pitch and yaw values based on mouse input
             _yaw += mouseX;
@@ -45,8 +38,18 @@ namespace SpaceMem.General
             // Limit the pitch to prevent the camera from flipping over
             _pitch = Mathf.Clamp(_pitch, -90.0f, 90.0f);
 
-            // Apply rotation
-            transform.eulerAngles = new Vector3(_pitch, _yaw, 0.0f);
+            // Apply rotation as local rotation to the transform
+            transform.localRotation = Quaternion.Euler(_pitch, _yaw, 0.0f);
+
+            // Move up and down
+            if (Input.GetKey(KeyCode.Q))
+            {
+                transform.localPosition += transform.up * speed * Time.deltaTime;
+            }
+            if (Input.GetKey(KeyCode.E))
+            {
+                transform.localPosition -= transform.up * speed * Time.deltaTime;
+            }
 
             Debug.DrawLine(transform.position, transform.position + transform.forward * 2.0f, Color.red);
         }
